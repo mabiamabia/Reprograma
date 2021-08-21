f5dc1f72dd9c584b27c0a04b63374608b52b0e9a
Priscila Duarte <duartecostap@gmail.com>
2021-08-21 11:13:12 -0300

    Insere arquivo dia 21-08-21

---
+++ 1 - AGOSTO/21-08-21/logica_JS1.js
@@ -0,0 +1,37 @@
+/*O que é Lógica de Programação?
+
+O que é Lógica: parte da filosofia que trata das formas do pensamento em geral (dedução, indução, hipótese, inferência etc.) e das operações intelectuais que visam à determinação do que é verdadeiro ou não.
+
+O que é programação: Programação é o processo de escrita, teste e manutenção de um programa de computador.
+*/
+
+// Como desenvolvemos a lógica de programação?
+
+//Linguagens e porque as utilizamos?
+
+//Criar Passo-a-Passo
+
+
+
+
+
+
+
+//Exercicio
+//Escreva um código na linguagem humana(natural) para trocar um pneu
+//Escreve um código na linguagem humana(natural) para abrir uma garrafa
+//Escreva um código na linguagem humana(natural) para arrumar uma cama
+//Escreva um código na linguagem humana(natural) para ligar uma televisão
+
+
+
+//QUAL LINGUAGEM VAMOS APRENDER? JAVASCRIPT
+//PORQUE?
+
+//COMO O JAVASCRIPT TRABALHA COM OUTRAS LINGUAGENS? 
+
+//INTEGRAÇÃO ENTRE AS LINGUAGENS
+
+//POSSO SUBSTITUIR O JAVASCRIPT POR OUTRA LINGUAGEM? PODE
+
+
