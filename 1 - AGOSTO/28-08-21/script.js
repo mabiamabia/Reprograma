658601ead17118b35b757d1ee4563cff318f1283
Priscila Duarte <duartecostap@gmail.com>
2021-08-28 13:33:15 -0300

    Update

---
+++ 1 - AGOSTO/28-08-21/script.js
@@ -0,0 +1,20 @@
+/* let num1 = prompt("Insira um número: ");
+let num2 = prompt("Insira um número: ");
+
+alert(num1 * num2);
+
+numero = prompt("Insira um numero qualquer: ")
+
+if(numero == 1 || numero == 2 || numero == 3) {
+    alert('o numero esta dentro do previsto')
+}else{
+    alert('O numero esta FORA do previsto')
+}; */
+
+var botao = document.getElementById('ola')
+
+function ola(){
+
+    alert("clicouuuuu")
+
+}
