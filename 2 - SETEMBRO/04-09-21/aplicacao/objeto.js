b90edba3d6c48fbe15e138324845fb5ab954bfc6
Priscila Duarte <duartecostap@gmail.com>
2021-09-04 16:34:01 -0300

    Insere Objeto

---
+++ 2 - SETEMBRO/04-09-21/aplicacao/objeto.js
@@ -0,0 +1,17 @@
+
+var personagem = [{
+    
+    nome:"Mabiá",
+    cabelos:"casatanho escuro",
+    altura:"1,72"
+},  
+{
+    nome: "E.T",
+    cabelos: "none",
+    altura: "1,43"
+
+}
+
+]
+
+console.log(Object.values(personagem))
